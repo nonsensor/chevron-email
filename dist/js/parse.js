6a14dcf94a3959ee1460f09387cdb72401a96379
mike <mikepropst@gmail.com>
2014-04-02 17:00:00 -0500

    big changes started for multi companies

--- dist/js/parse.js
+++ dist/js/parse.js
@@ -1,3 +1,5 @@
+/*
+
 function p(t){
     t = t.trim();
     return (t.length>0?'<p>'+t.replace(/[\r\n]+/,'</p><p>')+'</p>':null);
@@ -27,4 +29,28 @@ function writeStories(elNum,catContainer,xmlNode) {
 		catContainer.append('<div class="news-story" id="story-'+elNum+'"><div class="story-block"><h4></h4></div></div>');
 		$('#story-'+elNum+' h4').html(storyTitleText);
 		}$('#story-'+elNum+' h4').append("<small>&nbsp;&nbsp;"+sSource+"</small>");
+}
+*/
+
+// TODO
+// 1. grab the relevant information from each node.
+// 2. change HTML for top 3 vs the rest of the stories
+// 3. make sure the doneflag is working when you call other categories
+// 4. try to abstract this out so there isn't a function for every category
+
+function parseNews(xml) {
+	execActivityStories = $(xml).find("NewsDetails").has('CategoryText:contains("Executive Activity")');
+	execActivityStories.each(function(i){
+		if ($(this).find("doneflag").length==0) {
+			theTitle = $(this).find("TitleText").text();
+			buildStoryHTML(i,theTitle,"TestDate","Blah blah this is the summary");
+			$(this).append("<doneflag>Set</doneflag><br>");
+		}
+	});
+}
+
+function buildStoryHTML(numkey, storyTitle, storyDate, storySummary){
+	$("#amazon-stories").append('<div id="news-story'+numkey+'" class="news-story"><div class="story-block"><h2></h2><p></p></div></div>');
+	$("#news-story"+numkey+" h2").html(storyTitle);
+	$("#news-story"+numkey+" p").html(storySummary);
 }
