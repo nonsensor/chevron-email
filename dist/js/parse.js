8f5a91db4ec4c4a19afd60161293439b16f88d77
mike <mikepropst@gmail.com>
2014-04-03 00:22:29 -0500

    Made it work!!

--- dist/js/parse.js
+++ dist/js/parse.js
@@ -1,56 +1,60 @@
-/*
+var oCats = {
+	"New Product":"New Products",
+	"Executive Activity":"Executive Activities",
+	"Executive Quote":"Executive Quotes",
+	"New Alliance":"New Alliances",
+	"Regulation Or Litigation":"Regulations And Litigations",
+	"General Industry":"General Industry"
+};
 
-function p(t){
-    t = t.trim();
-    return (t.length>0?'<p>'+t.replace(/[\r\n]+/,'</p><p>')+'</p>':null);
-}
+function parseNews(xml,companyDiv,companyName) {
+	var dict = [];
+	$.each(oCats,function(key,val){
+		storyCat = val.toLowerCase();
+		storyCat = storyCat.split(" ");
+		storyCat = storyCat[storyCat.length-1];
+		relevantStories = $(xml).find("NewsDetails").has('CategoryText:contains("'+key+'")');
+		if(relevantStories.length>0) { $(companyDiv).append("<div class=\"row\"><div class=\"col-sm-12\"><h3 class=\"cat-title-"+storyCat+"\">"+companyName + ": "+val+"</h3></div></div>"); }
+		var count = 1;
+		relevantStories.each(function(i,ival){
+			var theTitle = $(ival).find("TitleText").text();
+			if($.inArray(theTitle,dict)==-1) { //add && i < 10 or whatever here if need be
+				var story = $(ival);
+				var theDate = story.find("PublishedDateTimestamp").text();
+				theDate = new Date(theDate);
+				var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+				theDate = monthNames[theDate.getMonth()] + " " + theDate.getDay();
+				var theSource = story.find("SourceDescription").text();
+				var theLink = story.find("WebPageURL").text();
+				var theContent = story.find("ContentText").text();
+				if(count<4) { //story with summary
+					buildStoryHTML(count,theTitle,theDate,theSource,theLink,theContent,companyDiv,companyName,storyCat);	
+				}
+				else { //headline only
+					buildHeadlineHTML(count,theTitle,theLink,companyDiv,companyName,storyCat);	
+				}				
+				dict.push(theTitle);
+				count++;
+			}
+		});
 
-function writeStories(elNum,catContainer,xmlNode) {
-	var storyNum = elNum.split("-").pop();
-	var sSource = xmlNode.find('SourceDescription').text();
-	var sLink = xmlNode.find('WebPageURL').text();
-	var sTitle = xmlNode.find('TitleText').text();
-	var sDate = xmlNode.find("PublishedDateTimestamp").text();
-	var sMoreLink = ' ... <a href="'+sLink+'">More »</a>';
-	var sContent = xmlNode.find('ContentText').text();
-	sContent = sContent.substr(0,160);
-	sDate = new Date(sDate);
-	var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-	sDate = monthNames[sDate.getMonth()] + " " + sDate.getDay();
-	var storyTitleText = '<a target="_blank" href="'+sLink+'">'+sTitle+'</a>';
-	if(storyNum<3) {
-		catContainer.append('<div class="news-story" id="story-'+elNum+'"><div class="story-block"><h2></h2><p class="meta"></p><div class="contents"></div></div></div>');
-		$('#story-'+elNum+' h2').html(storyTitleText);
-		$('#story-'+elNum+' .meta').html(sSource + " | " + sDate);
-		$('#story-'+elNum+' .contents').html(sContent);
-		$('#story-'+elNum+' .contents').append(sMoreLink);
-	}
-	else {
-		catContainer.append('<div class="news-story" id="story-'+elNum+'"><div class="story-block"><h4></h4></div></div>');
-		$('#story-'+elNum+' h4').html(storyTitleText);
-		}$('#story-'+elNum+' h4').append("<small>&nbsp;&nbsp;"+sSource+"</small>");
-}
-*/
-
-// TODO
-// 1. grab the relevant information from each node.
-// 2. change HTML for top 3 vs the rest of the stories
-// 3. make sure the doneflag is working when you call other categories
-// 4. try to abstract this out so there isn't a function for every category
-
-function parseNews(xml) {
-	execActivityStories = $(xml).find("NewsDetails").has('CategoryText:contains("Executive Activity")');
-	execActivityStories.each(function(i){
-		if ($(this).find("doneflag").length==0) {
-			theTitle = $(this).find("TitleText").text();
-			buildStoryHTML(i,theTitle,"TestDate","Blah blah this is the summary");
-			$(this).append("<doneflag>Set</doneflag><br>");
-		}
 	});
 }
 
-function buildStoryHTML(numkey, storyTitle, storyDate, storySummary){
-	$("#amazon-stories").append('<div id="news-story'+numkey+'" class="news-story"><div class="story-block"><h2></h2><p></p></div></div>');
-	$("#news-story"+numkey+" h2").html(storyTitle);
-	$("#news-story"+numkey+" p").html(storySummary);
+function buildStoryHTML(numkey, storyTitle, storyDate, storySource, storyLink, storySummary, companyDiv, companyName, storyCat){
+	$(companyDiv).find(".spinner").remove();
+	$(companyDiv).append('<div id="'+companyName+'-'+storyCat+'-story'+numkey+'" class="news-story full-story"><div class="story-block"><h4 class="story-date"></h4><h2></h2><p></p></div></div>');
+	var storyDiv = $("#"+companyName+"-"+storyCat+"-story"+numkey);
+	storyDiv.find("h4").html(storyDate);
+	storyDiv.find("h2").html(storyTitle);
+	storyDiv.find("p").html(storySummary);
+	//$("#news-story"+numkey+" p").html(storySummary);
+}
+
+function buildHeadlineHTML(numkey, storyTitle, storyLink, companyDiv, companyName, storyCat) {
+	$(companyDiv).append('<div id="'+companyName+'-'+storyCat+'-story'+numkey+'" class="summary"><div class="story-block"><p></p></div></div>');
+	
+	var storyDiv = $("#"+companyName+"-"+storyCat+"-story"+numkey);
+	var storyAndLink = '<a href="'+storyLink+'">'+storyTitle+'</a>';
+	storyDiv.find("p").html(storyAndLink);
 }
